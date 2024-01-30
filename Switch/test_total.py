a347d9e9054258419ec71735e49f4f2a860a4508
zeinabmaroufi <zeinab.maroufi@sinacomsys.com>
2024-01-30 02:08:47 -0800

    uncommit which features have been tested

--- Switch/test_total.py
+++ Switch/test_total.py
@@ -23,7 +23,7 @@ from config import *
 # from Switch.test_Bridge_stp_state_report import test_Bridge_Stp_report
 # from Switch.test_Interface_stp_state_report import test_interface_Stp_report
 # from Switch.test_Interface_Mstp_state_report import test_Port_Mstp_report
-from Switch.test_Port_Mirror_conf import test_Port_Mirror_config
+# from Switch.test_Port_Mirror_conf import test_Port_Mirror_config
 from Switch.test_IGMP_Configuration import test_IGMP_Configuration
 from Switch.test_Mapping import test_Mapping
 from Switch.test_Port_QinQ_conf import test_Port_QinQ_config
