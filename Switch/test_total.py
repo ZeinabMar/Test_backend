15852c13b1301e1f0f3df394b0f9d9578cf61c08
zeinabmaroufi <zeinab.maroufi@sinacomsys.com>
2024-01-29 23:00:15 -0800

    move report mstp interface upper than before

--- Switch/test_total.py
+++ Switch/test_total.py
@@ -22,10 +22,10 @@ from Switch.test_Port_Mstp_conf import test_Port_Mstp_config
 from Switch.test_Bridge_mstp_state_report import test_Bridge_Mstp_report
 from Switch.test_Bridge_stp_state_report import test_Bridge_Stp_report
 from Switch.test_Interface_stp_state_report import test_interface_Stp_report
+from Switch.test_Interface_Mstp_state_report import test_Port_Mstp_report
 from Switch.test_Port_Mirror_conf import test_Port_Mirror_config
 from Switch.test_IGMP_Configuration import test_IGMP_Configuration
 from Switch.test_Mapping import test_Mapping
-from Switch.test_Interface_Mstp_state_report import test_Port_Mstp_report
 from Switch.test_Port_QinQ_conf import test_Port_QinQ_config
 from Switch.test_Qos_policy_New_feature import test_Qos_Policy_config
 
